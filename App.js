ec4a9029b3dafeb8de248a551e7751a8943e0d60
unknown <aditaavram@yahoo.com>
2021-11-20 22:49:36 +0200

    Realm integrated&some bug fixes

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState, useEffect } from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+import Constants from "expo-constants";
+import AppNavigator from "./navigation/AppNavigator";
+// import { navigationRef } from "./NavigationService";
+
+import { Provider} from "react-redux";
+// import * as Permissions from "expo-permissions";
+
+
+import ReduxThunk from "redux-thunk";
+
+import { StyleSheet, Text, View } from "react-native";
+
+import movieReducer from "./store/reducers/fetchMovieData";
+import netReducer from "./store/reducers/internetStatus";
+import PushNotification from "react-native-push-notification";
+
+
+const rootReducer = combineReducers({movie:movieReducer,net:netReducer});
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- android/app/src/main/java/com/moviesapp/MainApplication.java
+++ android/app/src/main/java/com/moviesapp/MainApplication.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
